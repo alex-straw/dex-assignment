73a3f9f5d2158c6998a4d0802caba37160f60402
alex-straw <as17163@bristol.ac.uk>
2022-07-27 15:38:31 +0100

    init placeholder files

---
+++ dex-contracts/deploy/deploy.js
@@ -0,0 +1,11 @@
+/*
+
+How I would deploy this in production:
+
+Setup my hardhat-config-json to deploy to Rinkeby 
+
+Keep the owner's private key in a .dotenv file for a one time deployment
+
+I would use Alchemy / Infura as the RPC provider (and I would also keep these keys in the .dotenv)
+
+*/
---
+++ dex-contracts/test/dex.js
@@ -0,0 +1,5 @@
+/*
+
+    I would write Hardhat tests in here and run them with 'npx hardhat test'
+
+*/
